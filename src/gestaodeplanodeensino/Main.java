01a89fa5255990d7ece96f946dce61d496966632
João Pedro Segurado <Joo Pedro Segurado>
2016-02-18 11:04:40 -0200

    ImplementaÃÃo edicao item de ementa

--- src/gestaodeplanodeensino/Main.java
+++ src/gestaodeplanodeensino/Main.java
@@ -5,14 +5,12 @@ package gestaodeplanodeensino;
  * @author JoãoPedro
  */
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.logging.Level;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -26,25 +24,26 @@ public class Main {
     private static JSONArray listaDisciplinas = new JSONArray();
     
     public static void main(String[] args) {
+        System.out.println("Seja bem vindo ao Sistema de gestão de plano de ensino.\n");
         iniciar();
     }
     
     public static void iniciar(){
         listaDisciplinas = getAllDisciplinas();
-        System.out.println("Seja bem vindo ao Sistema de gestão de plano de ensino, por favor selecione uma opção:\n"
+        System.out.println("\n\nPor favor selecione uma opção:\n"
                 + "1 - cadastrar disciplina\n"
                 + "2 - listar disciplinas\n"
                 + "3 - editar disciplina\n"
                 + "4 - remover disciplina\n"
                 + "5 - gerar plano de ensino\n"
-                + "6 - sair");
+                + "6 - sair\n");
         
         Scanner scanner = new Scanner(System.in);
         String comando;
         int numeroComando = -1;
         
         try {
-            comando = scanner.next();
+            comando = scanner.nextLine();
             numeroComando = Integer.parseInt(comando);
         } catch (NumberFormatException e) {
         }
@@ -70,25 +69,25 @@ public class Main {
     public static void cadastrarDisciplina(){
         System.out.println("\nNome da disciplina:\n");
         Scanner scanner = new Scanner(System.in);
-        String nome = scanner.next();
+        String nome = scanner.nextLine();
         
         while(nome.isEmpty()){
             System.out.println("\n\nPor favor digite um nome válido...\n\n");
-            nome = scanner.next();
+            nome = scanner.nextLine();
         }
         
         System.out.println("\nDescrição da disciplina:\n");
-        String descricao = scanner.next();
+        String descricao = scanner.nextLine();
         
         while(descricao.isEmpty()){
             System.out.println("\n\nPor favor digite uma descrição válido...\n\n");
-            descricao = scanner.next();
+            descricao = scanner.nextLine();
         }
         
         System.out.println("\nCarga horária total da disciplina:\n");
         int cargaHoraria = -1;
         do{
-            String carga = scanner.next();
+            String carga = scanner.nextLine();
             try {
                 cargaHoraria = Integer.parseInt(carga);
             } catch (NumberFormatException e){
@@ -137,17 +136,173 @@ public class Main {
             }
             
             System.out.println(text.toString());
+            selecionarDisciplina();
         }else{
             System.out.println("\n\nNenhuma disciplina cadastrada \n\n");
+            iniciar();
         }
         
+        
+    }
+    
+    public static void selecionarDisciplina(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual disciplina deseja selecionar(digite o número da ordem):");
+        int ordem = -1;
+        do{
+            try{
+                ordem = Integer.valueOf(scanner.nextLine());
+            }catch(NumberFormatException e){}
+            if(ordem <= 0 || ordem > listaDisciplinas.length()){
+                System.out.println("\nPor favor, digite um valor válido\n");
+            }
+        }while(ordem <= 0 || ordem > listaDisciplinas.length());
+        imprimirDisciplina(ordem - 1);
+    };
+    
+    public static void imprimirDisciplina(int ordem){
+        JSONObject disciplina = listaDisciplinas.getJSONObject(ordem);
+        JSONArray itensDeEmenta = disciplina.getJSONArray(Contract.ITENS_DE_EMENTA);
+        StringBuilder text = new StringBuilder();
+        text.append("\n\nNome da disciplina: ").append(disciplina.get(Contract.NOME));
+                text.append("\nCarga horária da disciplina: ")
+                        .append(disciplina.get(Contract.CARGA_HORARIA))
+                        .append("h");
+        if(itensDeEmenta.length() > 0){
+            text.append("\n\nItens de ementa:");
+            for(int i=0; i<itensDeEmenta.length(); i++){
+                text.append("\n---------------------------------------------------");
+                text.append("\nItem de ementa ").append(i+1);
+                text.append("\nNome do item: ").append(itensDeEmenta.getJSONObject(i).get(Contract.NOME));
+                text.append("\nCarga horária: ")
+                        .append(itensDeEmenta.getJSONObject(i).get(Contract.CARGA_HORARIA))
+                        .append("h");
+                if(i == itensDeEmenta.length() - 1){
+                    text.append("\n---------------------------------------------------\n");
+                }
+            }
+        }
+        
+        System.out.println(text.toString());
+        
+        Scanner scanner = new Scanner(System.in);
+        int comando = -1;
+        System.out.println("Selecione uma opção:\n"
+                + "1 - editar item de ementa\n"
+                + "2 - deletar item de ementa\n"
+                + "3 - editar informações desta disciplina\n"
+                + "4 - voltar pro inicio\n");
+        do{
+            try {
+                comando = Integer.valueOf(scanner.nextLine());
+            }catch (NumberFormatException e) {}
+            if(comando <= 0 || comando > 4){
+                System.out.println("Por favor digite um comando válido.");
+            }
+        }while(comando <= 0 || comando > 4);
+        
+        
+        switch(comando){
+            case 1:
+                selecionarItemEmentaEditado(ordem);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                iniciar();
+                break;
+        }
+    };
+    
+    public static void selecionarItemEmentaEditado(int indiceDisciplina){
+        JSONObject disciplina = listaDisciplinas.getJSONObject(indiceDisciplina);
+        JSONArray itensDeEmenta = disciplina.getJSONArray(Contract.ITENS_DE_EMENTA);
+        StringBuilder text = new StringBuilder();
+       
+        
+        System.out.println(text.toString());
+        int comando = -1;
+        System.out.println("Por favor, digite o número de qual item de ementa deseja editar:");
+        do{
+            Scanner scanner = new Scanner(System.in);   
+            try{
+                comando = Integer.valueOf(scanner.nextLine());
+            }catch(NumberFormatException e){};
+            
+            if(comando <= 0 || comando > disciplina.getJSONArray(Contract.ITENS_DE_EMENTA).length()){
+                System.out.println("\nPor favor, digite um valor válido\n");
+            }
+            
+        }while(comando <= 0 || comando > disciplina.getJSONArray(Contract.ITENS_DE_EMENTA).length());
+        
+        editarItemDeEmenta(indiceDisciplina, comando - 1);
+    }
+    
+    public static void editarItemDeEmenta(int indiceDisciplina,int indiceItemEmenta){
+        JSONObject disciplina = listaDisciplinas.getJSONObject(indiceDisciplina);
+        JSONObject itemEmenta = disciplina.getJSONArray(Contract.ITENS_DE_EMENTA).getJSONObject(indiceItemEmenta);
+        
+        System.out.println("\nDigite o novo nome do item de ementa:\n");
+        Scanner scanner = new Scanner(System.in);
+        String nome = null;
+        do{
+            nome = scanner.nextLine();
+            if(nome.isEmpty()){
+                System.out.println("\nPor favor digite um nome válido");
+            }
+        }while(nome.isEmpty());
+        
+        int carga = -1;
+        System.out.println("Digite a nova carga horária do item de ementa:\n");
+        do{
+            carga = Integer.valueOf(scanner.nextLine());
+            if(carga <= 0){
+                System.out.println("\nPor favor digite um valor válido.");
+            }else if(carga > disciplina.getInt(Contract.CARGA_HORARIA)){
+                System.out.println("\nO valor da carga é maior que a carga da disciplina, tente novamnte:\n");
+                carga = -1;
+            }else{
+                int cargaTotal = 0;
+                for(int i=0; i<disciplina.getJSONArray(Contract.ITENS_DE_EMENTA).length(); i++){
+                    if(i == indiceItemEmenta){
+                        cargaTotal += carga;
+                    }else{
+                        cargaTotal += disciplina
+                                .getJSONArray(Contract.ITENS_DE_EMENTA)
+                                .getJSONObject(i).getInt(Contract.CARGA_HORARIA);
+                    }
+                }
+                
+                if(cargaTotal > disciplina.getInt(Contract.CARGA_HORARIA)){
+                    carga = -1;
+                    System.out.println("\nO A soma das cargas horárias dos itens de ementa é maior que a carga da disciplina, tente novamnte:\n");
+                }
+            }
+        }while(carga <= 0);
+        
+        itemEmenta.put(Contract.CARGA_HORARIA, carga);
+        itemEmenta.put(Contract.NOME, nome);
+        
+        disciplina.getJSONArray(Contract.ITENS_DE_EMENTA).put(indiceItemEmenta, itemEmenta);
+        listaDisciplinas.put(indiceDisciplina, disciplina);
+        try {
+            PrintWriter out = new PrintWriter("filename.txt");
+            out.println(listaDisciplinas.toString());
+            out.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        System.out.println("\nAlterado com sucesso\n");
         iniciar();
     }
     
     public static JSONArray getAllDisciplinas(){
         BufferedReader br;
         try {
-             br = new BufferedReader(new FileReader("filename.txt"));
+            br = new BufferedReader(new FileReader("filename.txt"));
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
 
@@ -159,7 +314,11 @@ public class Main {
             String json = sb.toString();
             br.close();
             
-            return new JSONArray(json);
+            if(json.length() <= 10){
+                return new JSONArray();
+            }else{
+                return new JSONArray(json);
+            }
             
         }catch (FileNotFoundException ex) {
             iniciarArquivo();
@@ -167,7 +326,7 @@ public class Main {
             System.out.println(ex.getMessage());
         }
         
-        return null;
+        return new JSONArray();
     }
     
     public static void salvarDisciplina(Disciplina d){
@@ -183,10 +342,10 @@ public class Main {
                 ItemDeEmenta item = d.getItens().get(i);
                 newObj.put(Contract.NOME, item.getNome());
                 newObj.put(Contract.CARGA_HORARIA, item.getCargaHoraria());
-                itens.put(newObj.toString());
+                itens.put(newObj);
             }
         }
-        obj.put(Contract.ITENS_DE_EMENTA, itens.toString());
+        obj.put(Contract.ITENS_DE_EMENTA, itens);
         
         listaDisciplinas.put(obj);
         
@@ -254,7 +413,7 @@ public class Main {
         String nome = null;
         
         do{
-            nome = scanner.next();
+            nome = scanner.nextLine();
             if(nome.isEmpty()){
                 System.out.println("\n\n Por favor digite um nome válido...\n\n");
             }
@@ -263,7 +422,7 @@ public class Main {
         String autor;
         System.out.println("\nDigite o nome do autor do livro:\n");
         do{
-            autor = scanner.next();
+            autor = scanner.nextLine();
             if(autor.isEmpty()){
                 System.out.println("\n\n Por favor digite um nome válido...\n\n");
             }
@@ -273,7 +432,7 @@ public class Main {
         
         System.out.println("\nDigite o nome da editora do livro:\n");
         do{
-            editora = scanner.next();
+            editora = scanner.nextLine();
             if(editora.isEmpty()){
                 System.out.println("\n\n Por favor digite um nome válido...\n\n");
             }
@@ -284,7 +443,7 @@ public class Main {
         
         System.out.println("\nDigite a edicao do livro:\n");
         do{
-            edicao = scanner.next();
+            edicao = scanner.nextLine();
             try {
                 numeroEdicao = Integer.parseInt(edicao);
             } catch (NumberFormatException e){
@@ -304,7 +463,7 @@ public class Main {
         String nome = null;
         
         do{
-            nome = scanner.next();
+            nome = scanner.nextLine();
             if(nome.isEmpty()){
                 System.out.println("\n\n Por favor digite um nome válido...\n\n");
             }
@@ -315,7 +474,7 @@ public class Main {
         
         System.out.println("\nDigite a carga horária do item de ementa:\n");
         do{
-            carga = scanner.next();
+            carga = scanner.nextLine();
             try {
                 cargaHorari = Integer.parseInt(carga);
             } catch (NumberFormatException e){
